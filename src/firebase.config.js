3a8bf6416b26ca39c3f13530a03c4102bc3b85d6
arainion <arainion21@gmail.com>
2022-03-02 07:14:25 +0200

    created structure

---
+++ src/firebase.config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCWpGsj-zeqVLKGpIuEsJ2MxvuTLU7wCjk',
+  authDomain: 'house-market-place-app-69e26.firebaseapp.com',
+  projectId: 'house-market-place-app-69e26',
+  storageBucket: 'house-market-place-app-69e26.appspot.com',
+  messagingSenderId: '136549724516',
+  appId: '1:136549724516:web:23c7ae0a2ac4241ffc8a71',
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore();
