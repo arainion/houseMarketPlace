bf6e9b44c5511bcf6bfb3ba380a6a33604ad3e29
arainion <arainion21@gmail.com>
2022-03-03 05:52:54 +0200

    implemented authentification + google

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { updateDoc, doc } from 'firebase/firestore';
 import { db } from '../firebase.config';
 import { getAuth, updateProfile } from 'firebase/auth';
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -11,6 +11,7 @@ import { setDoc, doc, serverTimestamp } from 'firebase/firestore';
 import { db } from '../firebase.config';
 import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg';
 import visibilityIcon from '../assets/svg/visibilityIcon.svg';
+import Oauth from '../components/Oauth';
 
 const SignUp = () => {
   const [showPassword, setShowPassword] = useState(false);
@@ -107,6 +108,8 @@ const SignUp = () => {
             </button>
           </div>
         </form>
+
+        <Oauth />
 
         <Link to="/sign-in" className="registerLink">
           Sign In Instead
